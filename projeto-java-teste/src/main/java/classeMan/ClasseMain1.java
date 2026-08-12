893bfc5e6a0e94ed0534804cba65aff27692c70a
Leonardo Maciel Bueno <leonardomb207@gmail.com>
2026-08-12 20:06:24 -0300

    Renomeia projeto para projeto-java-erp

--- projeto-java-teste/src/main/java/classeMan/ClasseMain1.java
+++ projeto-java-teste/src/main/java/classeMan/ClasseMain1.java
@@ -5,7 +5,7 @@ public class ClasseMain1 {
 	// Método que executa o codigo Java
 	public static void main(String[] args) {
 
-		System.out.println("Leonardo");
+		System.out.println("Leonardo é 100%");
 	}
 
 }
